f7fb586703c9e6ef1ec6bd0dd10491ebf59581d3
GEEKPLUS\zhen.liu <zhen.liu@geekplus.com>
2022-08-04 17:23:46 +0800

    chore: Initial

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,24 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/users",
+    name: "users",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/UsersView.vue"),
+  },
+  {
+    path: "/users/:id",
+    name: "user-detail",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/UserDetailView.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/UserDetailView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user-detail">
+    <h1>user id: {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+
+    console.log(route.params.id);
+
+    return {
+      id: route.params.id,
+    };
+  },
+});
+</script>
